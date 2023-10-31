5e372f6e21479bf8cfae5d02310b9797278a838a
Anusha Chitranshi <anusha.chitranshi@cern.ch>
2023-10-31 19:39:48 -0400

    Added Routing to Pages

--- src/App.jsx
+++ src/App.jsx
@@ -3,14 +3,14 @@ import "./App.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 import Account from "./Pages/Account";
-import Dashboard from "./Pages/Dashboard";
 import Announcements from "./Pages/Announcements";
+import Dashboard from "./Pages/Dashboard";
 import Garden from "./Pages/Garden";
 import Graphics from "./Pages/Graphics";
+import NoPage from "./Pages/NoPage";
 import SeniorDesign from "./Pages/SeniorDesign";
 import ToDo from "./Pages/ToDo";
 import UICourse from "./Pages/UICourse";
-import NoPage from "./Pages/NoPage";
 
 function App() {
   return (
