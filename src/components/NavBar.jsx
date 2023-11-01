2bdb9be2fa7f62ab776530531db73a92039ff3ad
Emma Gardner <egardner2019@gmail.com>
2023-11-01 15:53:40 -0400

    add favicon and navbar logo

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -7,17 +7,24 @@ import Navbar from "react-bootstrap/Navbar";
 const NavBar = () => {
   return (
     <Navbar bg="light" data-bs-theme="light">
-        <Container>
-          <Navbar.Brand href="/">Canvas</Navbar.Brand>
-          <Nav>
-            <Nav.Link href="/">Dashboard</Nav.Link>
-            <Nav.Link href="/garden">Garden</Nav.Link>
-            <Nav.Link href="/todo">To Do</Nav.Link>
-            <Nav.Link href="/announcements">Announcements</Nav.Link>
-            <Nav.Link href="/account">Account</Nav.Link>
-          </Nav>
-        </Container>
-      </Navbar>
+      <Container>
+        <Navbar.Brand href="/">
+          <img
+            alt="University of Cincinnati logo"
+            src="/University_of_Cincinnati_logo.png"
+            width="150"
+            className="d-inline-block align-top"
+          />
+        </Navbar.Brand>
+        <Nav>
+          <Nav.Link href="/">Dashboard</Nav.Link>
+          <Nav.Link href="/garden">Garden</Nav.Link>
+          <Nav.Link href="/todo">To Do</Nav.Link>
+          <Nav.Link href="/announcements">Announcements</Nav.Link>
+          <Nav.Link href="/account">Account</Nav.Link>
+        </Nav>
+      </Container>
+    </Navbar>
   );
 };
 
