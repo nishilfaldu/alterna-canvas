1952daecbcaeccd033bc3d8041e94dfef864cec7
Athulya Ganesh <athulya.ganesh2001@gmail.com>
2023-11-09 22:05:06 -0500

    some garden pictures

--- src/Pages/Garden.jsx
+++ src/Pages/Garden.jsx
@@ -1,5 +1,21 @@
-function Garden() {
-  return <h1>My Garden</h1>;
-}
+import image3 from "../assets/images/plant/garden3.png";
+import image4 from "../assets/images/plant/garden4.png";
+import image5 from "../assets/images/plant/garden5.png";
+import tomatostage4 from "../assets/images/plant/tomato/stage4.png";
+
+
+
+const Garden = () => {
+  return (
+    <>
+      <h1>My Garden</h1>
+      <hr></hr>
+      <h4>Progress So Far</h4>
+      <img src={image3} style={{ width: "75%", marginLeft: "auto", marginRight: "auto" }}></img>
+      <h4>Current</h4>
+      <img src={tomatostage4} style={{ width: "15%", marginLeft: "auto", marginRight: "auto" }}></img>
+    </>
+  );
+};
 
 export default Garden;
