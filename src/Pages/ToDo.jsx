9e6194d723cfe5432d4fdd4cfeadb65b022205cd
Emma Gardner <egardner2019@gmail.com>
2023-11-10 17:19:38 -0500

    fix error in to do components

--- src/Pages/ToDo.jsx
+++ src/Pages/ToDo.jsx
@@ -5,38 +5,33 @@ import CourseIcons from "../components/CourseIcons";
 import { useUser } from "../components/provider/useUser";
 import { getData } from "../scripts/jsonHelpers";
 
-
-
 function ToDo() {
   const { user } = useUser();
   const [assignmentsMeta, setAssignmentsMeta] = useState([]);
 
   useEffect(() => {
     async function getUserData() {
-      // if (!user) {
-      //   message.error("User not found");
+      if (user) {
+        const names = user.split(" ");
+        const firstName = names[0];
+        const lastName = names[1];
+        const userData = await getData(
+          `http://localhost:3030/students?name=${firstName}+${lastName}`
+        );
 
-      //   return;
-      // }
-      const names = user.split(" ");
-      const firstName = names[0];
-      const lastName = names[1];
-      const userData = await getData(
-        `http://localhost:3030/students?name=${firstName}+${lastName}`,
-      );
+        const courses = userData[0].courses;
 
-      const courses = userData[0].courses;
+        const assignmentsNotSubmitted = courses
+          .map((course) => course.tabs.assignments.assignmentsNotSubmitted)
+          .flat();
 
-      const assignmentsNotSubmitted = courses
-        .map(course => course.tabs.assignments.assignmentsNotSubmitted)
-        .flat();
+        // Sort the assignments in order of the due date
+        assignmentsNotSubmitted.sort((a, b) =>
+          a.dueDate > b.dueDate ? 1 : b.dueDate > a.dueDate ? -1 : 0
+        );
 
-      // Sort the assignments in order of the due date
-      assignmentsNotSubmitted.sort((a, b) =>
-        a.dueDate > b.dueDate ? 1 : b.dueDate > a.dueDate ? -1 : 0,
-      );
-
-      setAssignmentsMeta(assignmentsNotSubmitted);
+        setAssignmentsMeta(assignmentsNotSubmitted);
+      }
     }
 
     getUserData();
@@ -53,7 +48,11 @@ function ToDo() {
           <List.Item key={index}>
             <List.Item.Meta
               avatar={CourseIcons[item.class]}
-              title={<>{item.class}: {item.name}</>}
+              title={
+                <>
+                  {item.class}: {item.name}
+                </>
+              }
               description={item.dueDate}
             />
           </List.Item>
