13b00f3c6346cd1381a7bef117ffc7e2d42ff6cc
Zenith Zack <faldund@outlook.com>
2023-11-11 12:24:16 -0500

    fix(client): fix todo file path

--- src/Pages/Dashboard.jsx
+++ src/Pages/Dashboard.jsx
@@ -3,7 +3,7 @@ import { Container, Row, Col } from "react-bootstrap";
 import Announcements from "../components/dashboardComponents/Announcements";
 import CourseCard from "../components/dashboardComponents/CourseCard";
 import PlantStatus from "../components/dashboardComponents/PlantStatus";
-import Todo from "../components/dashboardComponents/ToDo";
+import Todo from "../components/dashboardComponents/Todo";
 import { useUser } from "../components/provider/useUser";
 
 
