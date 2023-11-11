9d9610240ec9c14c3a71424b3919c76b7b5bbaef
Emma Gardner <egardner2019@gmail.com>
2023-11-11 16:48:50 -0500

    stylize folder strings, hide metadata folder

--- src/scripts/tree.js
+++ src/scripts/tree.js
@@ -1,38 +1,37 @@
-
 export const createFolderToFileMap = fileList => {
-    const map = new Map();
+  const map = new Map();
 
-    for(let i = 0; i < fileList.length; i++) {
-        const partPaths = fileList[i].split("/");
-        const folderName = partPaths[partPaths.length - 2];
-        const fileName = partPaths[partPaths.length - 1];
+  for (let i = 0; i < fileList.length; i++) {
+    const partPaths = fileList[i].split("/");
+    const folderName = partPaths[partPaths.length - 2];
+    const fileName = partPaths[partPaths.length - 1];
 
-        if(map.has(folderName)) {
-            map.get(folderName).push(fileName);
-        } else if (!map.has(folderName)) {
-            map.set(folderName, []);
-        }
+    if (map.has(folderName)) {
+      map.get(folderName).push(fileName);
+    } else if (!map.has(folderName)) {
+      map.set(folderName, []);
     }
-    
-    return map;
+  }
+
+  return map;
 };
 
 export function createTreeStructure(folderToFileMap) {
-    let treeData = [];
+  let treeData = [];
 
-    // eslint-disable-next-line no-unused-vars
-    for (let [folderName, fileList] of folderToFileMap) {
-        treeData.push({
-            title: folderName,
-            key: folderName,
-            children: [],
-                // fileList.map(file => ({
-                //     title: file,
-                //     key: `${folderName}-${file}`,
-                //     isLeaf: true,
-                // })),
-        });
+  // eslint-disable-next-line no-unused-vars
+  for (let [folderName, fileList] of folderToFileMap) {
+    if (folderName !== "metadata") {
+      treeData.push({
+        title:
+          folderName === "course_info"
+            ? "Course Info"
+            : folderName.charAt(0).toUpperCase() + folderName.slice(1),
+        key: folderName,
+        children: [],
+      });
     }
+  }
 
-    return treeData;
+  return treeData;
 }
